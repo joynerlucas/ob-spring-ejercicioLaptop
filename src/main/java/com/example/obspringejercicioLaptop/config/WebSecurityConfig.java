bf0552ca3a924a4424c62e812a30eb0904fe0d2c
Jafet Crespo <jafetcrespo@gmail.com>
2022-02-08 22:15:48 -0400

    add system.properties

--- src/main/java/com/example/obspringejercicioLaptop/config/WebSecurityConfig.java
+++ src/main/java/com/example/obspringejercicioLaptop/config/WebSecurityConfig.java
@@ -27,6 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public HttpFirewall looseHttpFirewall(){
         StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowBackSlash(true);
         return firewall;
     }
 
